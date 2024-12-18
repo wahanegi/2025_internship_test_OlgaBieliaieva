b8f3fda8b5e441927f1ae04e6381ddece27ed06a
OlgaBieliaieva <belyaevaolya0@gmail.com>
2024-12-18 19:31:06 +0000

    Implemented letter opener for sending emails in dev mode

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :api do
     namespace :v1 do
       get "posts/index"
